2a53a030bdc5de60849ea26c8be31a2b92ddee29
Arthur Kunz <arthur.kunz@universo.univates.br>
2024-02-01 19:38:35 -0300

    feat: menu hamburguer added to the page

--- components/option-bar.vue
+++ components/option-bar.vue
@@ -1,51 +1,86 @@
 <template>
-  <section class="w-1/2 md:w-1/4 ml-10 mt-10">
-    <UVerticalNavigation :links="links" />
-  </section>
+  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div
+      class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+    >
+      <a
+        href="https://flowbite.com/"
+        class="flex items-center space-x-3 rtl:space-x-reverse"
+      >
+        <img
+          src="https://flowbite.com/docs/images/logo.svg"
+          class="h-8"
+          alt="Flowbite Logo"
+        />
+        <span
+          class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+          >Gerenciamento de Cabanas</span
+        >
+      </a>
+      <button>
+        <span class="sr-only">Open main menu</span>
+        <svg
+          @click="toggleNavBar"
+          class="w-5 h-5 mt-10 md:hidden"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+      <div
+        :class="navItem ? 'hidden' : 'block'"
+        class="w-full md:block md:w-auto"
+        id="navbar-default"
+      >
+        <ul
+          class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+        >
+          <li v-for="(path, index) in routePaths" :key="index">
+            <NuxtLink
+              :to="path.route"
+              class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >{{ path.name }}</NuxtLink
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script setup lang="ts">
-const route = useRoute();
-interface Link {
-  label: string;
-  icon?: string;
-  to?: string;
-  avatar?: {
-    src: string;
-  };
-}
-const links: Link[] = [
+const navItem = ref(true);
+const toggleNavBar = () => {
+  navItem.value = !navItem.value;
+};
+
+const routePaths = [
   {
-    label: "Perfil",
-    avatar: {
-      src: "https://avatars.githubusercontent.com/u/739984?v=4",
-    },
+    name: "Home",
+    route: "/",
   },
   {
-    label: "Home",
-    icon: "i-heroicons-home",
-    to: "/getting-started/installation",
-  },
-  {
-    label: "Agendamentos",
-    icon: "i-heroicons-chart-bar",
-    to: `${
-      route.path.startsWith("/dev") ? "/dev" : ""
-    }/components/vertical-navigation`,
-  },
-  {
-    label: "Cabanas",
-    icon: "i-heroicons-command-line",
-    to: "/cabins",
-  },
-  {
-    label: "Hóspedes",
-    icon: "i-heroicons-command-line",
+    name: "Cabanas",
+    route: "/cabins",
   },
 
   {
-    label: "Configurações",
-    icon: "i-heroicons-command-line",
+    name: "Clientes",
+    route: "/guests",
+  },
+
+  {
+    name: "Configurações",
+    route: "/settings",
   },
 ];
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   supabase: {
     redirect: false,
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
+    url: "https://nsccvdjgainhdemxgpeg.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zY2N2ZGpnYWluaGRlbXhncGVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQyOTg0NzEsImV4cCI6MjAxOTg3NDQ3MX0.xjaHV1UVeepU2uGNgCn-9T8xfJG7KBqk2LY9nvraWU4",
   },
 });
